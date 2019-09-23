090788ba40056ec5aeeeb488e33771a846596e48
Oleg Degay <Oleg_Degay@epam.com>
2019-09-22 21:57:42 -0400

    shoebill modification 092219

--- samp-install.sh
+++ samp-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
-downloadFiles()
+downloadBaseServer()
  {
-     wget  --user downloader --password QWErty123 http://35.206.126.225/artifactory/generic-local/reguhoop/sampserver/oroginalserver/samp037svr_R2-1.tar.gz
+     wget  http://35.206.126.225/artifactory/generic-local/reguhoop/sampserver/oroginalserver/samp037svr_R2-1.tar.gz
  }
   
- unpack()
+ unpackBaseServer()
  {
      tar -xf  samp037svr_R2-1.tar.gz
  }
@@ -17,16 +17,28 @@ downloadFiles()
  rconpass ()
  {
 	cd samp03 
-	sed -i 's/rcon_password changeme/rcon_password QWErty123/' server.cfg
-	cat server.cfg
+	sed -i 's/rcon_password changeme/rcon_password ABCdef123/' server.cfg
+	#cat server.cfg
  }
+ createShoebill()
+ {
+	wget  http://35.206.126.225/artifactory/generic-local/reguhoop/sampserver/shoebillbottstrap/shoebill.TAR
+	tar -xf  shoebill.TAR
+	cd shoebill
+	cd bootstrap
+	wget -O shoebill-dependency-manager.jar  http://35.206.126.225/artifactory/oxotarp_shoebill/net/gtaun/shoebill-dependency-manager/2.0/shoebill-dependency-manager-2.0.jar
+	wget -O shoebill-launcher.jar http://35.206.126.225/artifactory/oxotarp_shoebill/net/gtaun/shoebill-launcher/2.0/shoebill-launcher-2.0.jar
+	cd ..	
+ }
+ 
   
  main()
  {
      clean
-     downloadFiles
-     unpack
+     downloadBaseServer
+     unpackBaseServer
 	 rconpass
+	 createShoebill
      clean
 }
 main # calling program entry point
